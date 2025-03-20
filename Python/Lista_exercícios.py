40ee68fc6df68d86293acd6eb7313618cf413507
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-20 11:33:11 -0300

    atualização2.0

---
+++ Python/Lista_exercícios.py
@@ -0,0 +1,49 @@
+#Ex 1
+'''n1 = input("Diga um número \n->")
+n2 = input("Diga outro número \n->")
+if n1 > n2 :
+    print(n1)
+else :
+    print(n2)
+
+#Ex 2
+ano = int(input("Diga em que ano você nasceu.\n->"))
+if ano >= 2009 :
+    print("Não pode votar!!!")
+else:
+    print("Você já pode votar, vote com responsabilidade!!!")
+
+#Ex 3
+senha = input("Digite sua senha.\n->")
+if senha == "1234" :
+    print("ACESSO PERMITIDO")
+else :
+    print("ACESSO NEGADO")
+
+#Ex 4
+quantidade = int(input("Quantas maçãs você gostaria de comprar?\n->"))
+if quantidade < 12 :
+    print(f"Sua compra ficou no valor de R${0.30 * quantidade}")
+else :
+    print(f"Sua compra ficou no valor de R${0.25 * quantidade}")
+
+#Ex 5
+n1 = int(input("Diga um número.\n->"))
+n2 = int(input("Diga outro número.\n->"))
+n3 = int(input("Diga mais um número.\n->"))
+
+
+#Ex 6
+masculino = "2"
+feminino = "1"
+print("mulher ---> 1")
+print("homem ---> 2")
+sexo = input("Qual seu sexo?\n->")
+altura = float(input("Qual sua altura\n->"))
+if sexo == "2" :
+    print(f"Seu peso ideal é {(72.7 * altura)- 58}")
+if sexo == "1" :
+    print(f"Seu peso ideal é {(62.1 * altura)- 44.7}")
+    
+    
+#Ex 7'''
