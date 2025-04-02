c3e5cea716e7107630c73a7ad8e5a83ac473084f
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-02 09:24:17 -0300

    JavaScript

---
+++ Java-Script/Aula01.js
@@ -0,0 +1,8 @@
+// var ola = document.getElementById("laranja")
+
+// function abrir(){
+//     ola.textContent = "Hello World"
+// }
+
+console.log("Olá JavaScript!!!");
+document.querySelector("h1").style.color = "blue";
